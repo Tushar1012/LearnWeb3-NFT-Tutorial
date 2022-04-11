fb05249d9c14134cbf791827e95087c6dc71af15
Tushar <Tusharsolanki1011@gmail.com>
2022-04-11 19:17:48 +0530

    some commet added

--- contracts/NFTee.sol
+++ contracts/NFTee.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 // cryptopunk is  ERC721 signifies that the contract we are creating imports ERC721 and follows ERC721 contract from openzeppelin
+// this is form inheritance file need to understand
 contract CryptoPunk is ERC721 {
     constructor() ERC721("CryptoPunk","CYP"){
         //_mint to NFT to yourself
